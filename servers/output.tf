280321bb3a76dd8c02eb2ede83dacde5d5cc5b0d
Carlos H Simoes <caique.caique@gmail.com>
2022-01-23 22:23:16 -0300

    Validade outputs

--- servers/output.tf
+++ servers/output.tf
@@ -1,34 +1,34 @@
 output "private_ip" {
   value = {
     for instance in aws_instance.web:
-    instance.name => instance.private_ip
+    instance.private_ip => instance.private_ip
   }
 }
 
 output "public_dns" {
   value = {
     for instance in aws_instance.web:
-    instance.name => instance.public_dns
+    instance.public_dns => instance.public_dns
   }
 }
 
 output "public_ip" {
   value = {
     for instance in aws_instance.web:
-    instance.name => instance.public_ip
+    instance.public_ip => instance.public_ip
   }
 }
 
 output "id" {
   value = {
     for instance in aws_instance.web:
-    instance.name => instance.id
+    instance.id => instance.id
   }
 }
 
 output "arn" {
   value = {
     for instance in aws_instance.web:
-    instance.name => instance.arn
+    instance.arn => instance.arn
   }
 }
