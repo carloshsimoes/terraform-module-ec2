7b074657f07ddc770b6648e509d360eb022f6c30
Carlos H Simoes <caique.caique@gmail.com>
2022-08-13 11:11:05 -0300

    Add output to eip

--- servers/output.tf
+++ servers/output.tf
@@ -1,27 +1,41 @@
-output "private_ip" {
+output "instance_arns" {
+  value = {
+    for instance in aws_instance.web:
+    instance.id => instance.arn
+  }
+}
+
+output "instance_private_ip" {
   value = {
     for instance in aws_instance.web:
     instance.id => instance.private_ip
   }
 }
 
-output "public_dns" {
-  value = {
-    for instance in aws_instance.web:
-    instance.id => instance.public_dns
-  }
-}
-
-output "public_ip" {
+output "instance_public_ip" {
   value = {
     for instance in aws_instance.web:
     instance.id => instance.public_ip
   }
 }
 
-output "arn" {
+output "instance_public_dns" {
   value = {
     for instance in aws_instance.web:
-    instance.id => instance.arn
+    instance.id => instance.public_dns
+  }
+}
+
+output "eip_public_ip" {
+  value = {
+    for eip in aws_eip.servers:
+    eip.association_id => eip.public_ip
+  }
+}
+
+output "eip_public_dns" {
+  value = {
+    for eip in aws_eip.servers:
+    eip.association_id => eip.public_dns
   }
 }
