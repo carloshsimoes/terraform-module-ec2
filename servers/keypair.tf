b62e1d8c70012b603fb43fba220217cf91c7e342
Carlos H Simoes <caique.caique@gmail.com>
2022-08-13 09:31:10 -0300

    Fix keypair

--- servers/keypair.tf
+++ servers/keypair.tf
@@ -3,9 +3,10 @@ resource "tls_private_key" "this" {
 }
 
 module "key_pair" {
+  count  = var.create_keypair ? 1 : 0
   source = "terraform-aws-modules/key-pair/aws"
 
-  create_key_pair    = var.create_keypair
+  #create_key_pair    = var.create_keypair
   key_name           = var.key_name
   public_key         = tls_private_key.this.public_key_openssh
 }
